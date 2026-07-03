701fcd9b4ec723236f99f695043ba75953c43e5d
RANDRIANTSEHENO Tokiniaina <r.tokiniaina@outlook.com>
2026-07-03 16:25:01 +0300

    update data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -131,3 +131,8 @@ values
 insert into paiements (montant, date_paiement, id_mode_paiement, id_reservation)
 values
 (30000, '2026-07-02', 1, 1);
+
+insert into statut_reservation (libelle)
+values
+('confirmée'),
+('annulée');
