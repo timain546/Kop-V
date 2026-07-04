d18657b8a37cd9c7e230cb8fee5943c96cb09621
RANDRIANTSEHENO Tokiniaina <r.tokiniaina@outlook.com>
2026-07-04 11:41:55 +0300

    entities corrigé 3

--- transport/src/main/java/com/cooperative/transport/entities/PlaceStatut.java
+++ transport/src/main/java/com/cooperative/transport/entities/PlaceStatut.java
@@ -18,11 +18,11 @@ public class PlaceStatut {
 
     @ManyToOne
     @JoinColumn(name = "id", insertable = false, updatable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
-    private Place place;
+    private Places place;
 
     @ManyToOne
     @JoinColumn(name = "id_voyage", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
-    private Voyage voyage;
+    private Voyages voyage;
 
     @Column(name = "occupee", nullable = false)
     private Boolean occupee;
