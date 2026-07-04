76c1aa76868a455d0b36e31c917db21ef559940c
timain546 <timainrakotondranjato@gmail.com>
2026-07-04 10:41:09 +0300

    Initialisation 3

--- sql/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO etudiants(nom, semestre) VALUES
-    ('Rayan', 'S4'),
-    ('Toky', 'S4'),
-    ('Nathan', 'S2');
--- sql/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE kopv;
-\c kopv;
-
-CREATE TABLE etudiants (
-    id SERIAL PRIMARY KEY,
-    nom VARCHAR(50),
-    semestre VARCHAR(2)
-);
