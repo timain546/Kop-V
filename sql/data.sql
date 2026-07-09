f1540dedd7147beba773ac9ab0707e5ef02913ee
Arceus27 <daveramanantsoa@gmail.com>
2026-07-09 12:17:48 +0300

    data contrat et statut
    data contrat et statut

--- sql/data.sql
+++ sql/data.sql
@@ -53,3 +53,20 @@ INSERT INTO voyage_statut (id_voyage, id_statut, date_modification) VALUES
 (1, 2, '2026-06-01'),
 (2, 3, '2026-06-01');
 
+INSERT INTO salaires (id_employe, montant, date_modification) VALUES
+(1, 2000.00, '2026-01-01'),
+(2, 2500.00, '2026-01-01'),
+(3, 3000.00, '2026-01-01');
+(4, 3500.00, '2026-01-01');
+
+INSERT INTO contrats_employes (id_employe, date_embauche, date_renvoi) VALUES
+(1, '2026-01-01', NULL),
+(2, '2026-02-01', NULL),
+(3, '2026-03-01', NULL),
+(4, '2026-04-01', NULL);
+
+
+insert into statut_employe (libelle) values
+('Engagé'),
+('Renvoyé'),
+('Reambauché')
