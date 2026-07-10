0bd324080fde91c48b937bfaef340d16687a59fd
lanbpfv-tech <lanbpfv@gmail.com>
2026-07-10 04:15:43 +0300

    ajout marker de panne 1!

--- sql/data_09_07_2026.sql
+++ sql/data_09_07_2026.sql
@@ -10,10 +10,10 @@ INSERT INTO statut_reparation (libelle) VALUES
 ('en cours de depannage'),
 ('resolu');
 
-INSERT INTO pannes (id_voyage,id_chauffeur,date_signalement,id_motif_panne,description,id_statut_reparation_actuel) VALUES
-(1,4,'2026-07-10','1','Le moteur a surchauffé et le véhicule ne peut plus avancer.',1),
-(2,5,'2026-08-10','2','Une crevaison a été signalée sur le trajet.',1),
-(3,6,'2026-09-10','3','Problème électrique empêchant le démarrage du véhicule.',1);
+INSERT INTO pannes (id_voyage,id_chauffeur,date_signalement,lieu,id_motif_panne,description,id_statut_reparation_actuel) VALUES
+(1,4,'2026-07-10',ST_GeomFromText('POINT(48.2215 -18.9348)', 4326),'1','Le moteur a surchauffé et le véhicule ne peut plus avancer.',1),
+(2,5,'2026-08-10',ST_GeomFromText('POINT(47.4210 -19.2214)', 4326),'2','Une crevaison a été signalée sur le trajet.',1),
+(3,6,'2026-09-10',ST_GeomFromText('POINT(49.0500 -18.3100)', 4326),'3','Problème électrique empêchant le démarrage du véhicule.',1);
 
 INSERT INTO reparation (id_panne,id_statut_reparation,date_modification,cout) VALUES
 (1,1,'2026-07-10',NULL),
